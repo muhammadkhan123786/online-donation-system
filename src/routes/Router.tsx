21643cf4269254d4a65ecf42bdbd5c37170b4515
muhammadkhan123786 <muhammadkhan123829@gmail.com>
2025-08-07 13:53:14 +0500

    Fix: Added basename and 404.html for GitHub Pages

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -4,17 +4,22 @@ import DonationTypes from '../pages/Donation-Types/DonationTypes';
 import RootLayout from '../components/Layouts/RootLayout';
 import CustomDonation from '../pages/custom-donation/Custom-Donation';
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <RootLayout />,
+      children: [
+        { index: true, element: <Home /> },
+        {
+          path: '/donation-types',
+          element: <DonationTypes />,
+        },
+        { path: '/custom-donation', element: <CustomDonation /> },
+      ],
+    },
+  ],
   {
-    path: '/',
-    element: <RootLayout />,
-    children: [
-      { index: true, element: <Home /> },
-      {
-        path: '/donation-types',
-        element: <DonationTypes />,
-      },
-      { path: '/custom-donation', element: <CustomDonation /> },
-    ],
-  },
-]);
+    basename: '/online-donation-system', // ✅ this is important
+  }
+);
